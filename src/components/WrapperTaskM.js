b5bef19fe3e591c0e793c9f61179a24af2c6f951
AlexVila0204 <alberth@unitec.edu>
2024-05-14 20:14:23 -0600

    Faltaba el localstorage

--- src/components/WrapperTaskM.js
+++ src/components/WrapperTaskM.js
@@ -1,31 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormsTaskM } from "./FormsTaskM";
 import { v4 as uuidv4 } from "uuid";
 import { TaskManager } from "./TaskManager";
 uuidv4();
 export const WrapperTaskM = () => {
   const [taskMade, setTaskMade] = useState([]);
+
+  useEffect(() => {
+    const savedTask = JSON.parse(localStorage.getItem("taskMade")) || [];
+    setTaskMade(savedTask);
+  }, []);
+    
   const addTarea = (taskParameter) => {
     setTaskMade([
       ...taskMade,
       { id: uuidv4(), task: taskParameter, done: false },
     ]);
-    console.log(taskMade);
+    localStorage.setItem("taskMade", JSON.stringify(taskMade));
   };
   const deleteAllTaskMade = (id) => {
     setTaskMade([]);
+    localStorage.removeItem("taskMade");
   }
   const checkedTask = (id) => {
     const updatedTasks = taskMade.map((task) =>
       task.id === id ? { ...task, done: !task.done } : task
     );
     setTaskMade(updatedTasks);
-    console.log(taskMade);
+    localStorage.setItem("taskMade", JSON.stringify(updatedTasks));
   }
   const deleteCompleted = () => {
     const updatedTasks = taskMade.filter((task) => !task.done);
     setTaskMade(updatedTasks);
+    localStorage.setItem("taskMade", JSON.stringify(updatedTasks));
   }
+
   return (
     <div className="WrapperTaskM">
       <h1>Tareas</h1>
